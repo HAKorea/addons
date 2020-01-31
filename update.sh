355844065f09466e5d0297eb412bfea51044c83b
Zoolian <me@zooil.com>
2020-01-31 21:13:43 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@
 DIRS=`ls -d ./*/|sed 's/\.//g'|sed 's/\///g'`
   
 for i in ${DIRS};do
-      echo "$i";
-`git subtree pull --prefix=$i $i master`	
+        echo "$i";
+	echo `git subtree pull --prefix=$i $i master`	
 done
