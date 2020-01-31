b0531a08731566e2578dad178f6ba488f41887a1
Zoolian <me@zooil.com>
2020-01-31 15:41:11 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 SHARE_DIR=/share/kocom_wallpad
 
-/makeconf.sh
 if [ ! -f $SHARE_DIR/rs485.py ]; then
 	mkdir $SHARE_DIR
 	mv /rs485.py $SHARE_DIR
 fi
+/makeconf.sh
 
 echo "[Info] Run Wallpad Controller"
 python3 $SHARE_DIR/rs485.py
