a22cd98f8748b6c0886a606f264cbd1478002bc3
Zoolian <me@zooil.com>
2020-02-02 12:28:20 +0900

    major

--- run.sh
+++ run.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-CUSTOM_FILE=$(jq --raw-output ".customfile" $CONFIG_PATH)
-MODEL=$(jq --raw-output ".model" $CONFIG_PATH)
-
-JS_FILE=$MODEL".js"
 CONFIG_PATH=/data/options.json
 SHARE_DIR=/share
+
+CUSTOM_FILE=$(jq --raw-output ".customfile" $CONFIG_PATH)
+MODEL=$(jq --raw-output ".model" $CONFIG_PATH)
+JS_FILE=$MODEL"_wallpad.js"
 
 if [ -f $SHARE_DIR/$CUSTOM_FILE ]; then
 	echo "[Info] Initializing with Custom file: "$CUSTOM_FILE
 	JS_FILE=$CUSTOM_FILE
 else
   	if [ ! -f $SHARE_DIR/$JS_FILE ]; then
-        	mv /js/$JS_FILE $SHARE_DIR
+		LS_RESULT=`ls $SHARE_DIR | grep wallpad`
+		if [ $? -eq 0 ]; then
+			rm $SHARE_DIR/*wallpad.js
+		fi
+        	cp /js/$MODEL".js" $SHARE_DIR/$JS_FILE
 	fi
 fi
 
@@ -22,4 +26,4 @@ echo "[Info] Wallpad Controller stand by... : "$JS_FILE
 JS_FILE=/$SHARE_DIR/$JS_FILE
 node $JS_FILE
 
-while true; do echo "still live"; sleep 1800; done
+#while true; do echo "still live"; sleep 1800; done
