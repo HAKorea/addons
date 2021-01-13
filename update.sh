8e796193b59a72c8f5aadedc712ce1a82c62e619
Zoolian <m@zooil.com>
2021-01-13 20:37:24 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -16,12 +16,14 @@ for i in ${DIRS};do
 
         array_check=`in_array $i ${READTREE_REPO[@]}`
         if [ "${array_check}" == "true" ]; then
-#	       echo `rm -rf $i` 
-#              echo `git read-tree --prefix=$i -u $i:$i`
-               echo "read-tree"
+	      	echo `rm -rf $i` 
+		echo `git add -A`
+		echo `git commit -m $i" deleted"`	
+              	echo `git read-tree --prefix=$i -u $i:$i`
+              	echo "read-tree"
         else
-               echo `git subtree pull --prefix=$i $i master`
-               echo "subtree"
+              	echo `git subtree pull --prefix=$i $i master`
+              	echo "subtree"
         fi
 done
 
